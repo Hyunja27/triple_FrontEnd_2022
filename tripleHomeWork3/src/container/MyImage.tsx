29d4a93a17a823a28a6648f3666e2a5a9c864deb
hyunja27 <123gk@naver.com>
2022-06-30 12:12:08 +0900

    :construction: 06/30_constructing_mainPage_text_and_animation_effect #5

--- tripleHomeWork3/src/container/MyImage.tsx
+++ tripleHomeWork3/src/container/MyImage.tsx
@@ -1,7 +1,7 @@
-// import profile from '../img/profile.png'
-// import bg from '../img/img-01-bg.png'
-// import { styled, leftFadeIn } from '../theme/theme'
-import { styled } from '../theme/theme'
+import profile from '../img/profile.png'
+import bg from '../img/img-01-bg.png'
+import { styled, leftFadeIn } from '../theme/theme'
+// import { styled } from '../theme/theme'
 
 const ResumeZone = styled('section', {
   height: '88vh',
@@ -9,28 +9,53 @@ const ResumeZone = styled('section', {
   position: 'relative',
 })
 
-// const BackgroundImage = styled('img', {
-//   height: '88vh',
-//   width: '100vw',
-//   objectFit: 'fill',
-// })
+const BackgroundImage = styled('img', {
+  height: '88vh',
+  width: '100vw',
+  objectFit: 'fill',
+})
 
-// const ImageContainer = styled('img', {
-//   width: '550px',
-//   height: '520px',
-//   position: 'absolute',
-//   top: '35vh',
-//   left: '10vw',
-//   animation: `${leftFadeIn} 700ms`,
-//   animationDelay: '100ms',
-// })
+const ImageContainer = styled('img', {
+  width: '550px',
+  height: '520px',
+  position: 'absolute',
+  top: '35vh',
+  left: '10vw',
+  animation: `${leftFadeIn} 700ms`,
+  animationDelay: '100ms',
+})
+
+const TextContainer = styled('section', {
+  top: '35vh',
+  left: '50vw',
+  textAlign: 'start',
+  position: 'absolute',
+})
+
+const MyImageText = styled('p', {
+  fontSize: '36px',
+  fontFamily: 'sans-serif',
+  animation: `${leftFadeIn} 700ms`,
+  animationDelay: '800ms',
+  animationFillMode: 'backwards',
+})
+
+const WeightText = styled('span', {
+  fontWeight: 'bold',
+})
 
 export default function Image() {
   return (
-    // <ResumeZone>
-    //   <BackgroundImage src={bg} alt="triple Logo" />
-    //   <ImageContainer src={profile} alt="triple Logo" />
-    // </ResumeZone>
-    <ResumeZone />
+    <ResumeZone>
+      <BackgroundImage src={bg} alt="triple Logo" />
+      <ImageContainer src={profile} alt="triple Logo" />
+      <TextContainer>
+        <MyImageText>
+          오해하지 마세요. <br />
+          <WeightText>술은 적당히만 마신답니다.</WeightText>
+        </MyImageText>
+      </TextContainer>
+    </ResumeZone>
+    // <ResumeZone />
   )
 }
