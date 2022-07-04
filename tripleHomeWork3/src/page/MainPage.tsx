c885181f7237a2ccf6de04b75d8f41b618cb6aad
hyunja27 <123gk@naver.com>
2022-07-04 17:40:51 +0900

    :memo: 07/04_Fix_annotation_message #8

--- tripleHomeWork3/src/page/MainPage.tsx
+++ tripleHomeWork3/src/page/MainPage.tsx
@@ -1,6 +1,8 @@
 import MarketingInfo from '../container/MarketingInfo'
 // import BonusImage from '../container/bonus/BonusImage'
 
+// BonusImage 컴포넌트는 과제와 직접적 관계가 없는 보너스 화면구성 컴포넌트입니다.
+
 export default function MainPage() {
   return (
     <section>
